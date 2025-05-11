560c9ea688f223c4d9cba02da397bd71c9b56d6b
Sabine Schleise <45167220+sabinecelina@users.noreply.github.com>
2025-05-12 00:58:31 +0200

    edge_tts

--- text-to-speech.py
+++ text-to-speech.py
@@ -1,10 +1,10 @@
-# import asyncio
-# import edge_tts
+import asyncio
+import edge_tts
 
 
-# async def speak(text):
-#     communicate = edge_tts.Communicate(text, voice="de-DE-KatjaNeural")
-#     await communicate.save("output_2.mp3")
+async def speak(text):
+    communicate = edge_tts.Communicate(text, voice="de-DE-KatjaNeural")
+    await communicate.save("output_edge_ttes.mp3")
 
 text = """Hallo und herzlich willkommen. Dies ist ein Testtext zur Überprüfung der Sprachsynthese. 
 Achte auf die Betonung, die Aussprache und den natürlichen Sprachfluss. Zahlen wie eins, zwei, drei 
@@ -12,10 +12,10 @@ sollten klar verständlich sein. Auch schwierige Wörter wie ‚Sprachsyntheseko
 ‚Entwicklungsperspektiven‘ werden jetzt getestet. Zum Schluss folgt ein kurzer Satz mit 
 emotionaler Betonung: Ich freue mich sehr, dass du heute hier bist!"""
 
-# asyncio.run(speak(text))
+asyncio.run(speak(text))
 
 
-from gtts import gTTS
+# from gtts import gTTS
 
-tts = gTTS(text=text, lang='de')
-tts.save("output.mp3")
+# tts = gTTS(text=text, lang='de')
+# tts.save("output.mp3")
